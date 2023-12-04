f2c615771adfcf4c9e9c135d77acf1c208229f92
liondungl <lidungl@archimedia.at>
2023-12-04 20:24:46 +0100

    Added Day 4

---
+++ 2023/day4_scratchcards.py
@@ -0,0 +1,27 @@
+#####################################################
+# Advent of Code 2023, Day four: "Scratchcards _"   #
+# https://adventofcode.com/2023/day/4               #
+#####################################################
+    
+import os
+    
+# get input ------------------------------------------------------------------------------------------------------------
+    
+with open(os.path.join('inputs', 'day4.txt'), 'r') as f:
+    entries = f.readlines()
+    
+# functions for part 1 and part 2 of the puzzle ------------------------------------------------------------------------
+
+
+def part1(entry_list):
+    pass
+    
+    
+def part2(entry_list):
+    pass
+
+# Solutions ------------------------------------------------------------------------------------------------------------
+
+
+# print(f'Solution part 1: {part1(entries)}')
+# print(f'Solution part 2: {part2(entries)}')
