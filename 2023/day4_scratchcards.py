1c89699e5bcb3fe983b1cc51e31f3bfb63783e81
liondungl <lidungl@archimedia.at>
2023-12-04 20:27:05 +0100

    add day 4

--- 2023/day4_scratchcards.py
+++ 2023/day4_scratchcards.py
@@ -4,6 +4,8 @@
 #####################################################
     
 import os
+import re
+import numpy as np
     
 # get input ------------------------------------------------------------------------------------------------------------
     
@@ -13,15 +15,27 @@ with open(os.path.join('inputs', 'day4.txt'), 'r') as f:
 # functions for part 1 and part 2 of the puzzle ------------------------------------------------------------------------
 
 
-def part1(entry_list):
-    pass
-    
-    
-def part2(entry_list):
-    pass
+def solve(entry_list):
+    result_1 = 0
+    cards_counter = np.ones(len(entry_list))
+    for n, card in enumerate(entry_list):
+        card = card.replace('  ', ' ')
+        _, numbers = card.split(':')
+        winning_n, my_n = numbers.strip().split('|')
+        winning_n, my_n = np.array(winning_n.strip().split(' '), dtype=int), np.array(my_n.strip().split(' '), dtype=int)
+        n_matches = np.sum(list(map(lambda x: x in winning_n, my_n)))
+
+        if n_matches > 0:  # part 1
+            result_1 += np.power(2, n_matches-1)
+
+        cards_counter[n + 1:n + 1 + n_matches] += cards_counter[n]  # part 2
+        result_2 = int(np.sum(cards_counter))
+
+    return result_1, result_2
+
 
 # Solutions ------------------------------------------------------------------------------------------------------------
 
-
-# print(f'Solution part 1: {part1(entries)}')
-# print(f'Solution part 2: {part2(entries)}')
+results = solve(entries)
+print(f'Solution part 1: {results[0]}')
+print(f'Solution part 2: {results[1]}')
